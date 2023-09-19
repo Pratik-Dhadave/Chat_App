17894c0dfbc1743abe81b098d965ddc653e0fd39
Pratik_Dhadave <pratik007ddd@gmail.com>
2023-09-20 00:09:47 +0530

    connection between send & reciver is done using socket.io

--- server.js
+++ server.js
@@ -4,10 +4,23 @@ const http = require('http').createServer(app)
 
 const PORT = process.env.PORT || 3000
 
-http.listen(PORT, ()=>{console.log(`listening on port ${PORT}`)})
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
+})
+
+// Socket 
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+    console.log('Connected...')
+    socket.on('message', (msg) => {
+        socket.broadcast.emit('message', msg)
+    })
+
 })
